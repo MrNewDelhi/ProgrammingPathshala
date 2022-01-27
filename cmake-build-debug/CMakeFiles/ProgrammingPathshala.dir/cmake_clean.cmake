366b9f6ab21bf0a4263c7a2c3da748a326fcc80c
anmol <anmolsoin1@gmail.com>
2022-01-28 03:53:34 +0530

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/ProgrammingPathshala.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProgrammingPathshala.dir/main.cpp.obj"
+  "ProgrammingPathshala.exe"
+  "ProgrammingPathshala.exe.manifest"
+  "ProgrammingPathshala.pdb"
+  "libProgrammingPathshala.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProgrammingPathshala.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
